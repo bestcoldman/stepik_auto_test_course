29f0839d67a07553661b2b1314813b8be0ac2f9b
Oleg Melnikov <olezhka.melnikov.1993@mail.ru>
2022-12-25 17:54:01 +0300

    added .py files

---
+++ Stepik_autotest.py
@@ -0,0 +1,34 @@
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+    confirm = browser.switch_to.alert
+    confirm.accept()
+    x_element = browser.find_element(By.CSS_SELECTOR, "[id='input_value']")
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element(By.CSS_SELECTOR, "[id='answer']")
+    input1.send_keys(y)
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    print(browser.switch_to.alert.text)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+
+# не забываем оставить пустую строку в конце файла
